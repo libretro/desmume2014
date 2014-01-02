ee799894c609e68079bd668153954ac1319d7ef0
twinaphex <libretro@gmail.com>
2014-01-02 18:57:40 +0100

    commit r4648 - add hacks for certain games whose save size autodetection is always doomed to fail

--- src/mc.cpp
+++ src/mc.cpp
@@ -469,10 +469,13 @@ void BackupDevice::reset_command()
 				break;
 			case 3:
 				//another modern typical case..
-				//but unfortunately we select this case for spider-man 3, when what it meant to do was
-				//present the archaic 1+2 case
-				//it seems that over the hedge does this also.
-				addr_size = 2;
+            //but unfortunately we select this case on accident sometimes when what it meant to do was present the archaic 1+2 case
+            //(the archaic 1+2 case is: specifying one address byte, and then reading the first two bytes, instead of the first one byte, as most other games would do.)
+            //so, we're gonna hack in checks for the games that are doing this
+ 				addr_size = 2;
+				if(!memcmp(gameInfo.header.gameCode,"AL3E",4)) addr_size = 1; //Spongebob Atlantis Squarepants.
+				if(!memcmp(gameInfo.header.gameCode,"AH5E",4)) addr_size = 1; //Over The Hedge 
+				if(!memcmp(gameInfo.header.gameCode,"AQ3E",4)) addr_size = 1; //Spider-Man 3
 				break;
 			case 4:
 				//a modern typical case
